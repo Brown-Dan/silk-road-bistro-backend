52ece059a9133d9fab5bcaa89f39085801ba244d
daniel.brown <daniel.brown@autotrader.co.uk>
2024-06-26 13:49:13 +0100

    project init

---
+++ src/main/java/uk/danbrown/apprenticeshipchineserestaurantbackend/ApprenticeshipChineseRestaurantBackendApplication.java
@@ -0,0 +1,13 @@
+package uk.danbrown.apprenticeshipchineserestaurantbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprenticeshipChineseRestaurantBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApprenticeshipChineseRestaurantBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/danbrown/apprenticeshipchineserestaurantbackend/ApprenticeshipChineseRestaurantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.danbrown.apprenticeshipchineserestaurantbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprenticeshipChineseRestaurantBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
