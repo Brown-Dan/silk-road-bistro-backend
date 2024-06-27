f4fff852b837f3a8a4f29854ec9cec96127b5a87
daniel.brown <daniel.brown@autotrader.co.uk>
2024-06-27 08:49:58 +0100

    added test for articleMapper

--- src/test/java/uk/danbrown/apprenticeshipchineserestaurantbackend/repository/mapper/ArticleMapperTest.java
+++ src/test/java/uk/danbrown/apprenticeshipchineserestaurantbackend/repository/mapper/ArticleMapperTest.java
@@ -4,9 +4,18 @@ import io.cucumber.java.bs.A;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.co.autotrader.generated.tables.pojos.ArticleEntity;
+import uk.danbrown.apprenticeshipchineserestaurantbackend.domain.Article;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static uk.danbrown.apprenticeshipchineserestaurantbackend.domain.Article.Builder.anArticle;
 
 public class ArticleMapperTest {
 
+    private static final String TITLE = "title";
+    private static final String CONTENT = "content";
+    private static final LocalDate DATE = LocalDate.of(2024, 6, 27);
 
     private ArticleMapper articleMapper;
 
@@ -15,14 +24,26 @@ public class ArticleMapperTest {
         articleMapper = new ArticleMapper();
     }
 
-
     @Test
     void toDomain_givenValidArticleEntity_shouldReturnArticle() {
+        Article result = articleMapper.toDomain(getArticleEntity());
+
+        assertThat(result).isEqualTo(getArticle());
+    }
+
+    private ArticleEntity getArticleEntity() {
         ArticleEntity articleEntity = new ArticleEntity();
+        articleEntity.setTitle(TITLE);
+        articleEntity.setContent(CONTENT);
+        articleEntity.setDate(DATE);
+        return articleEntity;
+    }
 
-        articleEntity.setTitle("title");
-        articleEntity.setContent("content");
-
-
+    private Article getArticle() {
+        return anArticle()
+                .withTitle(TITLE)
+                .withContent(CONTENT)
+                .withDate(DATE)
+                .build();
     }
 }
