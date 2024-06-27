1f9942c482587e4f84ca5d1569f02eacb49af2ab
daniel.brown <daniel.brown@autotrader.co.uk>
2024-06-27 08:58:53 +0100

    added tests for ExceptionHandlers

---
+++ src/test/java/uk/danbrown/apprenticeshipchineserestaurantbackend/controller/ExceptionHandlersTest.java
@@ -0,0 +1,50 @@
+package uk.danbrown.apprenticeshipchineserestaurantbackend.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+import uk.danbrown.apprenticeshipchineserestaurantbackend.controller.error.Error;
+import uk.danbrown.apprenticeshipchineserestaurantbackend.controller.error.ErrorResponse;
+import uk.danbrown.apprenticeshipchineserestaurantbackend.exception.EntityAlreadyExistsWithIdException;
+import uk.danbrown.apprenticeshipchineserestaurantbackend.exception.FailureInsertingEntityException;
+
+import java.util.Collections;
+
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ExceptionHandlersTest {
+
+    private ExceptionHandlers exceptionHandlers;
+
+    @BeforeEach
+    void setUp() {
+        exceptionHandlers = new ExceptionHandlers();
+    }
+
+    @Test
+    void handleFailureInsertingEntity_givenException_shouldReturnResponseEntity() {
+        FailureInsertingEntityException exception = new FailureInsertingEntityException(null);
+
+        ResponseEntity<ErrorResponse> expectedResult = ResponseEntity
+                .status(500)
+                .body(new ErrorResponse(singletonList(Error.failureInsertingEntity("Failed to insert entity - null"))));
+
+        ResponseEntity<ErrorResponse> result = exceptionHandlers.handleFailureInsertingEntity(exception);
+
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    void handleEntityAlreadyExistsWithId_givenException_shouldReturnResponseEntity() {
+        EntityAlreadyExistsWithIdException exception = new EntityAlreadyExistsWithIdException("ID");
+
+        ResponseEntity<ErrorResponse> expectedResult = ResponseEntity
+                .status(409)
+                .body(new ErrorResponse(singletonList(Error.entityAlreadyExistsWithId("Entity already exists with Id - 'ID'"))));
+
+        ResponseEntity<ErrorResponse> result = exceptionHandlers.handleEntityAlreadyExistsWithId(exception);
+
+        assertThat(result).isEqualTo(expectedResult);
+    }
+}
